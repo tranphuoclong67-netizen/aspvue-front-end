84cc1815947fbe3e93c08d423cb89467ad7312c0
tranphuoclong67-netizen <tranphuoclong67@gmail.com>
2026-05-04 12:48:11 +0700

    push front end to main

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-﻿const BASE_URL = 'http://localhost:5035'
+﻿const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5035'
 
 // Dùng memory thay localStorage để tránh lỗi extension
 const store = {
