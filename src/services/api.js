1ef390098422888856dfd64151a884c21c218b8e
tranphuoclong67-netizen <tranphuoclong67@gmail.com>
2026-05-07 22:05:00 +0700

    lastest

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-﻿const BASE_URL = 'https://aspvue-backend.onrender.com'
+﻿const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5035';
+
 const store = {
     token: null,
     username: null,
